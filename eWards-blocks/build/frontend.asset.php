a23b7b6526fdf4401e9d403a9e6afe39b183f413
vinyasbj <vinyas555@gmail.com>
2023-12-01 17:21:35 +0530

    Ewards Blocks added

---
+++ eWards-blocks/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '9c055d5082dfd7c221cf');
---
+++ eWards-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '11a1a349cd5584fa81e4');
